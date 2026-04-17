4f042fa48212fc17fe7e23a29c4cb6d4b5ab6051
itdevhiro-glitch <itdevhiro@gmail.com>
2026-04-17 16:21:28 +0700

    Add files via upload

---
+++ script/admin.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvL7vHx8cXGbBIpUOIF9cbLSNMeSO9cdo",
+  authDomain: "aquaacademy-ae7a5.firebaseapp.com",
+  databaseURL: "https://aquaacademy-ae7a5-default-rtdb.firebaseio.com",
+  projectId: "aquaacademy-ae7a5",
+  storageBucket: "aquaacademy-ae7a5.firebasestorage.app",
+  messagingSenderId: "851048143100",
+  appId: "1:851048143100:web:52fb802804f9fda9e65f79",
+  measurementId: "G-4LVK266PY2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // Validasi ulang: jika bukan admin, tendang ke dashboard biasa
+    if (!user.email.endsWith('@admin.service')) {
+      alert("Akses Ditolak! Anda bukan administrator.");
+      window.location.href = 'dashboard.html';
+    } else {
+      document.getElementById('adminEmail').innerText = user.email;
+    }
+  } else {
+    window.location.href = 'login.html';
+  }
+});
+
+document.getElementById('btnLogout').addEventListener('click', () => {
+  signOut(auth).then(() => {
+    window.location.href = 'login.html';
+  });
+});
